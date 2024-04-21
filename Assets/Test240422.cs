3470d40bd78e2aecbdcd6eefd34b0871774cf2a7
haruking53 <167697631+haruking53@users.noreply.github.com>
2024-04-22 00:54:28 +0900

    Update Test240422.cs

--- Assets/Test240422.cs
+++ Assets/Test240422.cs
@@ -7,7 +7,7 @@ public class Test240422 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        aaa
     }
 
     // Update is called once per frame
